3e34d56d4f4c57e9dacbd877470a3ad195aec597
Seu Nome <seuemail@email.com>
2026-05-15 15:47:23 -0300

    feat: endpoint GET /usuarios funcionando

--- src/main/java/com/daniel/crud_spring/controller/UsuarioControleer.java
+++ src/main/java/com/daniel/crud_spring/controller/UsuarioControleer.java
@@ -1,5 +1,7 @@
 package com.daniel.crud_spring.controller;
 
+
+import com.daniel.crud_spring.repository.UsuarioRepository;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,9 +9,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 @RestController
 @RequestMapping("/usuarios")
 public class UsuarioControleer {
-
+    private final UsuarioRepository usuarioRepository;
+    public UsuarioControleer(UsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
     @GetMapping
-    public String listar() {
-        return "Funcionando!";
+    public Object listar() {
+        return usuarioRepository.findAll();
     }
 }
--- src/main/java/com/daniel/crud_spring/model/Usuario.java
+++ src/main/java/com/daniel/crud_spring/model/Usuario.java
@@ -14,15 +14,18 @@ public class Usuario {
 
 
     public String getNome(){
+
         return nome;
     }
     public void setNome(String nome){
+
         this.nome=nome;
     }
     public Long getId(){
         return id;
     }
     public void setId(Long id){
+
         this.id=id;
     }
 }
