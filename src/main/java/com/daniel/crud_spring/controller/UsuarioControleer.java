b145f6c98f5f5e0a73edf828d65d2664eeb0f6c9
Seu Nome <seuemail@email.com>
2026-05-15 16:13:15 -0300

    feat : criando endpoints GET e POST da API

--- src/main/java/com/daniel/crud_spring/controller/UsuarioControleer.java
+++ src/main/java/com/daniel/crud_spring/controller/UsuarioControleer.java
@@ -1,10 +1,11 @@
 package com.daniel.crud_spring.controller;
-
-
+import org.springframework.web.bind.annotation.RequestBody;
+import com.daniel.crud_spring.model.Usuario;
 import com.daniel.crud_spring.repository.UsuarioRepository;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @RestController
 @RequestMapping("/usuarios")
@@ -16,5 +17,9 @@ public class UsuarioControleer {
     @GetMapping
     public Object listar() {
         return usuarioRepository.findAll();
+    }
+    @PostMapping
+    public Usuario cadastrar(@RequestBody Usuario usuario) {
+        return usuario;
     }
 }
